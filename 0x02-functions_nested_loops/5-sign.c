34417ffc4c674b6d35b94648131053915b777354
mohamedmoo <mhefny66@gmail.com>
2023-06-20 18:02:16 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,14 +4,14 @@
  * @n: the number to be checked
  * Return: 1 for positive num, -1 for negative num or zero for anything alse
  */
-int print_sign(int n)
+int print_sign(int n);
 {
 	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
 	}
-	else if (n <0)
+	else if (n < 0)
 	{
 		_putchar(45);
 		return (-1);
